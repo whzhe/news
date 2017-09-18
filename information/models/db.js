2b6f2e379786b4fea191f3493ed9141a12812f4e
gjz22cn <gjz22cn@hotmail.com>
2017-09-18 18:32:01 +0800

    update for db

--- information/models/db.js
+++ information/models/db.js
@@ -4,4 +4,4 @@ var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 
 
-module.exports = new Db(settings.db, new Server(settings.host, settings.PORT, {}));
+module.exports = new Db(settings.db, new Server(settings.host, settings.port, {}));
--- information/models/settings.js
+++ information/models/settings.js
@@ -1,9 +1,9 @@
 module.exports = {
-    COOKIE_SECRET: 'information.com',
-    URL: 'mongodb://127.0.0.1:27017/nodedb',
-    DB: 'information', 
-    HOST: '127.0.0.1',
-    PORT: 27017,
-    USERNAME: 'root',
-    PASSWORD: '1234'
+    cookie_secret: 'information.com',
+    url: 'mongodb://127.0.0.1:27017/nodedb',
+    db: 'information', 
+    host: '127.0.0.1',
+    port: 27017,
+    username: 'root',
+    password: '1234'
 };
