62914e20250d1d17e904a890f935ee7152ecc9fc
gjz22cn <gjz22cn@hotmail.com>
2017-09-18 18:19:29 +0800

    update for db connection

---
+++ information/models/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    COOKIE_SECRET: 'information.com',
+    URL: 'mongodb://127.0.0.1:27017/nodedb',
+    DB: 'information', 
+    HOST: '127.0.0.1',
+    PORT: 27017,
+    USERNAME: 'root',
+    PASSWORD: '1234'
+};
